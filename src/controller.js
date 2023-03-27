d2ca6e1154139ed01e28d132b285d5612ccad59b
Jose Wills <jose.wills3@gmail.com>
2023-03-27 09:41:34 +0100

    ships in viewport

--- src/controller.js
+++ src/controller.js
@@ -26,6 +26,13 @@
                 portsElement.style.width = `${portsElementWidth + 256}px`;
             });
         }
+        renderShip(ship) {
+            const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+            const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+            const shipElement = document.querySelector('#ship');
+            shipElement.style.top = `${portElement.offsetTop + 30}px`;
+            shipElement.style.left = `${portElement.offsetLeft - 30}px`;
+        }
     }
     if (typeof module !== 'undefined' && module.exports) {
         module.exports = Controller;
