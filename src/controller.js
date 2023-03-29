c1daf97a5b468c38dca2f315348bf6074e9501e1
Jose Wills <jose.wills3@gmail.com>
2023-03-29 22:43:41 +0100

    ship sets sail

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,11 @@
 (function exportController() {
     class Controller {
-        constructor() {
+        constructor(ship) {
+            this.ship = ship;
             this.initialiseSea();
+            document.querySelector('#sailButton').addEventListener('click', () => {
+                this.setSail();
+            })
         }
         initialiseSea() {
             const backgrounds = [
@@ -15,6 +19,7 @@
         renderPorts(ports) {
             const portsElement = document.querySelector('#ports');
             portsElement.style.width = '0px';
+
             ports.forEach((port, index) => {
                 const newPortElement = document.createElement('div');
                 newPortElement.className = 'port';
@@ -26,12 +31,32 @@
                 portsElement.style.width = `${portsElementWidth + 256}px`;
             });
         }
-        renderShip(ship) {
+        renderShip() {
+            const ship = this.ship;
             const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
             const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
             const shipElement = document.querySelector('#ship');
-            shipElement.style.top = `${portElement.offsetTop + 30}px`;
+            shipElement.style.top = `${portElement.offsetTop - 54}px`;
             shipElement.style.left = `${portElement.offsetLeft - 30}px`;
+        }
+        setSail() {
+            const ship = this.ship;
+            const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+            const nextPortIndex = currentPortIndex + 1;
+            const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+            if (!nextPortElement) {
+                return alert('End of the line!');
+              }
+            const shipElement = document.querySelector('#ship');
+            const sailInterval = setInterval(() => {
+                const shipLeft = parseInt(shipElement.style.left, 10);
+                if (shipLeft === (nextPortElement.offsetLeft - 30)) {
+                    ship.setSail();
+                    ship.dock();
+                    clearInterval(sailInterval);
+                }
+                shipElement.style.left = `${shipLeft + 1}px`;
+            }, 20);
         }
     }
     if (typeof module !== 'undefined' && module.exports) {
